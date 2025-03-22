ff7ecf3dd613da5e9993d0b52b2f2cb8e723b868
A-Estevez <52750278+A-Estevez@users.noreply.github.com>
2025-03-22 13:47:49 +0100

    all ratios named
    generated all combinations of ratios in a 40 card deck with unique alfabetical codes for each 'unique card'

--- og_shuffler.py
+++ og_shuffler.py
@@ -13,7 +13,7 @@ t = time.process_time()
 deck_len = 40
 deck = np.array(range(deck_len))
 #print(deck)
-itrs = 1000000					
+itrs = 1000					
 decks_sh = []
 
 
@@ -25,6 +25,5 @@ for i in range(itrs):
 
 #print(decks_sh)
 np.savetxt("all_decks_40.csv", decks_sh, delimiter=",", fmt='%s')
-#https://stackoverflow.com/questions/20033458/python-rename-files-reading-names-from-csv-file
 elapsed_time = time.process_time() - t
 print(elapsed_time)
--- ratio_to_card.py
+++ ratio_to_card.py
@@ -28,22 +28,34 @@ deck_01 = []
 round = 0
 template = df_ratios.iloc[0]
 print(template)
-while round < template.loc['ca2']:
-    deck_01.append(all_names_40[round])
-    deck_01.append(all_names_40[round])
-    round +=1
-else:
-    while round < template.loc['ca2'] + template.loc['ca3']:
-        deck_01.append(all_names_40[round])
-        deck_01.append(all_names_40[round])
-        deck_01.append(all_names_40[round])
-        round +=1
+def df_to_ratio_list(template): #defines a function to get the ratios from a row of df_ratios
+    round = 0
+    deck_01 = []
+    while round < template.loc['ca1'] + template.loc['ca2'] + template.loc['ca3']:
+        if round <template.loc['ca1']:
 
-print(deck_01)
-print(len(deck_01))
-print(len(np.unique(deck_01)))
-print(template.loc['cunicas'])
-print(len(np.unique(deck_01)) == template.loc['cunicas'])
+            deck_01.append(all_names_40[round])
+            round +=1
+        elif round < template.loc['ca1'] + template.loc['ca2']:
+            deck_01.append(all_names_40[round])
+            deck_01.append(all_names_40[round])
+
+            round +=1
+
+        elif round < template.loc['ca1'] + template.loc['ca2'] + template.loc['ca3']:
+            deck_01.append(all_names_40[round])
+            deck_01.append(all_names_40[round])
+            deck_01.append(all_names_40[round])
+            round +=1
+    return deck_01
+print(df_to_ratio_list(template))  #test the function
+all_ratios_40_name = []
+for index, row in df_ratios.iterrows():
+    all_ratios_40_name.append(df_to_ratio_list(row))
+print(all_ratios_40_name)
+
+np.savetxt("all_ratios_40_name.csv", all_ratios_40_name, delimiter=",", fmt='%s')
+
 
 deck_nums = list(range(0,41))
 cards_numbered_temp = list(itertools.chain.from_iterable(zip(deck_01, deck_nums)))
