037559c958c7098399497384d0b5ac138ba6fdcb
A-Estevez <52750278+A-Estevez@users.noreply.github.com>
2025-03-26 09:05:51 +0100

    finished changes in all ratios named
    each deck starts with a unique numerical description of the ratios

--- ratio_to_card.py
+++ ratio_to_card.py
@@ -4,14 +4,11 @@ import string
 import itertools
 #simbolos < > 
 
-#####
-#gets a csv of all possible ratios for a 40 card deck
-#and transforms it into a csv of all possible ratios with unique names for each unique card
-#####
-
 df_ratios = pd.read_csv('all_ratios_40.csv',index_col=False,  names = ('a1', 'ca1', 'a2', 'ca2', 'a3', 'ca3', 'cunicas'))
+df_decks = pd.read_csv('all_decks_40.csv', header = None)
 
 #print(df_ratios)
+#print(df_decks.columns)
 
 alfabet = list(string.ascii_lowercase)
 #print(alfabet)
@@ -30,11 +27,13 @@ all_names_40 = alfbt_dupt[0:40]
 deck_01 = []
 round = 0
 template = df_ratios.iloc[0]
-#print(template)
+print(template)
 def df_to_ratio_list(template): #defines a function to get the ratios from a row of df_ratios
     round = 0
     deck_01 = []
+    deck_01.append([template.loc['ca1'], template.loc['ca2'], template.loc['ca3']])
     while round < template.loc['ca1'] + template.loc['ca2'] + template.loc['ca3']:
+        
         if round < template.loc['ca1']:
 
             deck_01.append(all_names_40[round])
@@ -51,12 +50,11 @@ def df_to_ratio_list(template): #defines a function to get the ratios from a row
             deck_01.append(all_names_40[round])
             round +=1
     return deck_01
-#print(df_to_ratio_list(template))  #test the function
+print(df_to_ratio_list(template))  #test the function
 all_ratios_40_name = []
 for index, row in df_ratios.iterrows():
-    print(df_ratios.iloc[row]['ca1'].values[0], df_ratios.iloc[row]['ca2'].values[0], df_ratios.iloc[row]['ca3'].values[0])
-    all_ratios_40_name.append([df_to_ratio_list(row), [df_ratios.iloc[row]['ca1'].values[0], df_ratios.iloc[row]['ca2'].values[0], df_ratios.iloc[row]['ca3'].values[0]]])
-#print(all_ratios_40_name)
+    all_ratios_40_name.append(df_to_ratio_list(row))
+print(all_ratios_40_name)
 
 np.savetxt("all_ratios_40_name.csv", all_ratios_40_name, delimiter=",", fmt='%s')
 
@@ -67,4 +65,4 @@ cards_numbered_temp = list(itertools.chain.from_iterable(zip(deck_01, deck_nums)
 
 
 cards_numbered = list(itertools.islice(cards_numbered_temp, 2))
-#print(cards_numbered)
+print(cards_numbered)
